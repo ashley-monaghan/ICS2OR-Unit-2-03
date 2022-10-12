289340d86122a32ba0217b675db544aa63e20875
ashley-monaghan <ashley-monaghan@users.noreply.github.com>
2022-10-12 18:35:49 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@
  * This function gets users first name and age and shows it back to user.
  */
 function enterClicked() {
-    // input
-    const firstName = document.getElementById("first-name").value
-    const userAge = parseInt(document.getElementById("age-entered").value)
-    
-    // output
+  // input
+  const firstName = document.getElementById("first-name").value
+  const userAge = parseInt(document.getElementById("age-entered").value)
+
+  // output
   document.getElementById("address").innerHTML =
-  "Your info is: " + firstName + ", age " + userAge + "."
+    "Your info is: " + firstName + ", age " + userAge + "."
 }
